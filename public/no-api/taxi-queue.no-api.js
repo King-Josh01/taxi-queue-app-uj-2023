a1982e86ae426335e4a269e2d9cb6b2a7d915f33
King-Josh01 <onoriodejoshua.91@gmail.com>
2023-08-24 12:07:31 +0200

    first commit

--- public/no-api/taxi-queue.no-api.js
+++ public/no-api/taxi-queue.no-api.js
@@ -4,32 +4,39 @@ document.addEventListener('alpine:init', () => {
 
 		return {
 			version: 'no-api-1.0',
+			TaxiCount: 0,
+			PassengerCount: 0,
 			joinQueue() {
-
+				this.PassengerCount++;
 			},
+
 			leaveQueue() {
+				this.PassengerCount--;
 
 			},
 
 			joinTaxiQueue() {
-
+				this.TaxiCount++;
 			},
 
 			queueLength() {
-
-			},
-
+				return this.PassengerCount;
+			}
+			,
 			taxiQueueLength() {
-
-			},
-
+				return this.TaxiCount;
+			}
+			,
 			taxiDepart() {
 
+				if (this.PassengerCount >= 12 && this.TaxiCount > 0) {
+					this.PassengerCount -= 12;
+					this.TaxiCount--;
+
+				}
 			}
 		}
 
 	});
 
 });
-
-
